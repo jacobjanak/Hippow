c16ce90d81ab8168f166e998d0e328f0fed74028
Jacob Janak <jakeathotmail@gmail.com>
2021-04-16 20:29:35 -0600

    getting firestore to work on heroku

--- server.js
+++ server.js
@@ -2,12 +2,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
+const admin = require("firebase-admin");
 const jwt = require("jsonwebtoken");
 const sha256 = require("js-sha256");
-
-// database
-const admin = require("firebase-admin");
-const sdk = require("./firebase-adminsdk.json");
 
 // values
 const imageURL = "https://ispy-beta.herokuapp.com";
@@ -29,8 +26,24 @@ app.use((req, res, next) => {
     next();
 })
 
-// initialize database
-admin.initializeApp({ credential: admin.credential.cert(sdk) })
+// database
+if (process.env.project_id) {
+    admin.initializeApp({ credential: admin.credential.cert({
+        project_id: process.env.project_id,
+        type: process.env.type,
+        private_key_id: process.env.private_key_id,
+        private_key: process.env.private_key,
+        client_email: process.env.client_email,
+        client_id: process.env.client_id,
+        auth_uri: process.env.auth_uri,
+        token_uri: process.env.token_uri,
+        auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+        client_x509_cert_url: process.env.client_x509_cert_url,
+    })})
+} else {
+    const sdk = require("./firebase-adminsdk.json");
+    admin.initializeApp({ credential: admin.credential.cert(sdk) })
+}
 const db = admin.firestore();
 
 // load blockchain
